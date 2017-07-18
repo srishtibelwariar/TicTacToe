566eccc2c264d478701efde0a62d26e8d9ffa1f3
Srishti Belwariar <sb2355@cornell.edu>
2017-07-17 22:41:49 -0400

    Add files via upload

--- ttt.py
+++ ttt.py
@@ -62,34 +62,40 @@ Builder.load_string("""
         
         Button:
             text:"0"
+            font_size: '50sp'
             size: 2*root.width/9, root.height/3
             pos: 0, 2*root.height/3+10
             id: 0
         Button:
             text:"1"
+            font_size: '50sp'
             size: 2*root.width/9 -10, root.height/3
             pos: 2*root.width/9 +10, 2*root.height/3+10
             id: 1
     
         Button:
             text:"2"
+            font_size: '50sp'
             size: 2*root.width/9 -10, root.height/3
             pos: 4*root.width/9+10, 2*root.height/3+10
             id: 2
             
         Button:
             text:"3"
+            font_size: '50sp'
             size: 2*root.width/9, root.height/3 -10
             pos: 0, 1*root.height/3+10
             id: 3
         Button:
             text:"4"
+            font_size: '50sp'
             size: 2*root.width/9 -10, root.height/3 -10
             pos: 2*root.width/9 +10, 1*root.height/3+10
             id: 4
     
         Button:
             text:"5"
+            font_size: '50sp'
             size: 2*root.width/9 -10, root.height/3 -10
             pos: 4*root.width/9+10, 1*root.height/3+10
             id: 5
@@ -97,16 +103,19 @@ Builder.load_string("""
         
         Button:
             text:"6"
+            font_size: '50sp'
             size: 2*root.width/9 , root.height/3
             pos: 0, 0
             id: 6
         Button:
             text:"7"
+            font_size: '50sp'
             size: 2*root.width/9 -10 , root.height/3
             pos: 2*root.width/9 +10, 0
             id: 7
         Button:
             text:"8"
+            font_size: '50sp'
             size: 2*root.width/9 -10, root.height/3
             pos: 4*root.width/9+10, 0
             id: 8
@@ -123,6 +132,16 @@ Builder.load_string("""
             size: 200, 200
             pos: 7*root.width/9+10, 20
             on_press: app.stop()
+        
+        Label:
+            text:"Player 1's turn!"
+            font_size: '30sp'
+            halign: 'right'
+            valign: 'middle'
+            pos: 7*root.width/9+10, root.height/2
+            id: message
+            
+        
 """)
 
 class MenuScreen(Screen):
