b9b076cb3df075019945649ee6af3ca0aa0dd4fb
Srishti Belwariar <sb2355@cornell.edu>
2017-07-19 15:42:44 -0400

    Add files via upload

--- ttt.py
+++ ttt.py
@@ -1,163 +1,45 @@
 #!/usr/bin/env python
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.vector import Vector
 from kivy.uix.screenmanager import ScreenManager
 from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import WipeTransition
+from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.properties import NumericProperty
+from kivy.properties import ReferenceListProperty
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 
-Builder.load_string("""
-<MenuScreen>:
-    GridLayout:
-        cols: 2
-        rows: 2
-        
-        GridLayout:
-            rows:2
-            Label:
-                text: '3x3 Tic Tac Toe'
-                font_size: '40sp'
-            Label:
-                text: 'Please select game type and click start'
-                valign: 'middle'
-        GridLayout:
-            rows:3
-            ToggleButton:
-                text: 'Human vs Human'
-                group: 'player'
-                state: 'down'
-            ToggleButton:
-                text: 'Human vs Random Computer'
-                group: 'player'
-            ToggleButton:
-                text: 'Human vs Smart Computer'
-                group: 'player'
-        Button:
-            text: 'Quit'
-            on_press: app.stop()
-        Button:
-            text: 'Start'
-            on_press:
-                root.manager.transition.direction = 'left'
-                root.manager.current = 'game'
+class TTTTile(Widget):
+    name= StringProperty(" ")
+    def new(self):
+        if self.name==" ":
+            self.name= "X"
 
-<GameScreen>:
-    canvas:
-        Rectangle:
-            pos: 2*self.width /9,0
-            size: 10, self.height
-        Rectangle:
-            pos: 4*self.width /9, 0
-            size: 10, self.height
-        Rectangle:
-            pos: 2*self.width /3, 0
-            size: 2, self.height
-        Rectangle:
-            pos: 0, self.height/3
-            size: 2*self.width/3, 10
-        
-        Rectangle:
-            pos: 0, 2*self.height/3
-            size: 2*self.width/3, 10
-        
-    GridLayout:
-        
-        Button:
-            text:"0"
-            font_size: '50sp'
-            size: 2*root.width/9, root.height/3
-            pos: 0, 2*root.height/3+10
-            id: 0
-        Button:
-            text:"1"
-            font_size: '50sp'
-            size: 2*root.width/9 -10, root.height/3
-            pos: 2*root.width/9 +10, 2*root.height/3+10
-            id: 1
+class TTTGame(Widget):
+    T0= ObjectProperty(None)
+    T1= ObjectProperty(None)
+    T2= ObjectProperty(None)
+    T3= ObjectProperty(None)
+    T4= ObjectProperty(None)
+    T5= ObjectProperty(None)
+    T6= ObjectProperty(None)
+    T7= ObjectProperty(None)
+    T8= ObjectProperty(None)
     
-        Button:
-            text:"2"
-            font_size: '50sp'
-            size: 2*root.width/9 -10, root.height/3
-            pos: 4*root.width/9+10, 2*root.height/3+10
-            id: 2
-            
-        Button:
-            text:"3"
-            font_size: '50sp'
-            size: 2*root.width/9, root.height/3 -10
-            pos: 0, 1*root.height/3+10
-            id: 3
-        Button:
-            text:"4"
-            font_size: '50sp'
-            size: 2*root.width/9 -10, root.height/3 -10
-            pos: 2*root.width/9 +10, 1*root.height/3+10
-            id: 4
-    
-        Button:
-            text:"5"
-            font_size: '50sp'
-            size: 2*root.width/9 -10, root.height/3 -10
-            pos: 4*root.width/9+10, 1*root.height/3+10
-            id: 5
-        
-        
-        Button:
-            text:"6"
-            font_size: '50sp'
-            size: 2*root.width/9 , root.height/3
-            pos: 0, 0
-            id: 6
-        Button:
-            text:"7"
-            font_size: '50sp'
-            size: 2*root.width/9 -10 , root.height/3
-            pos: 2*root.width/9 +10, 0
-            id: 7
-        Button:
-            text:"8"
-            font_size: '50sp'
-            size: 2*root.width/9 -10, root.height/3
-            pos: 4*root.width/9+10, 0
-            id: 8
-        Button:
-            text:"Home Menu"
-            size: 200, 200
-            pos: 7*root.width/9+10, 240
-            on_press:
-                root.manager.transition.direction = 'right'
-                root.manager.current = 'menu'
-            
-        Button:
-            text:"Quit Game"
-            size: 200, 200
-            pos: 7*root.width/9+10, 20
-            on_press: app.stop()
-        
-        Label:
-            text:"Player 1's turn!"
-            font_size: '30sp'
-            halign: 'right'
-            valign: 'middle'
-            pos: 7*root.width/9+10, root.height/2
-            id: message
-            
-        
-""")
+    def update(self):
+        pass
 
-class MenuScreen(Screen):
+class Manager(ScreenManager):
     pass
 
-class GameScreen(Screen):
-    pass
 
-screens = ScreenManager()
-screens.add_widget(MenuScreen(name='menu'))
-screens.add_widget(GameScreen(name='game'))
-
-class tttApp(App):
-
+class TTTApp(App):
     def build(self):
-        return screens
+        self.load_kv('ttt.kv')
+        return Manager()
 
 if __name__ == '__main__':
-    tttApp().run()
+    TTTApp().run()
